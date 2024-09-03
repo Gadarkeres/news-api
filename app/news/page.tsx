2896758a694e3c6fa3488980b9e25af33c28c33e
Matheus Rafael <matheusrafael77@hotmail.com>
2024-09-02 23:18:48 -0300

    server component in page

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -2,8 +2,8 @@ import { Header } from "@/components/header/header";
 import NewsContainer from "@/components/news-container/news-container";
 import { Suspense } from "react";
 
-export default function Page() {
-  //simular carregamento
+export default async function Page() {
+  await new Promise((resolve) => setTimeout(resolve, 2500));
   return (
     <>
       <Suspense fallback={<div>Carregando ...</div>}>
--- components/news-container/news-container.tsx
+++ components/news-container/news-container.tsx
@@ -1,8 +1,6 @@
 import News from "./news";
 
-export default async function NewsContainer() {
-  await new Promise((resolve) => setTimeout(resolve, 2500));
-
+export default function NewsContainer() {
   return (
     <section className="min-w-full min-h-screen flex flex-col justify-center items-center lg:flex-row flex-wrap lg:gap-5 mt-5">
       {Array.from({ length: 6 }).map((_, index) => (
